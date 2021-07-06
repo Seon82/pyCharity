26ea23e384cd75e8f2a643dcac31b51e62794d9b
Seon <>
2021-07-06 08:59:48 +0200

    Further optimized detemplatize

--- src/handlers/pxls/detemplatize.pyx
+++ src/handlers/pxls/detemplatize.pyx
@@ -1,9 +1,11 @@
 import numpy as np
 cimport numpy as np
 import cython
+from cython.parallel cimport prange
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
+@cython.cdivision(True)
 def fast_detemplatize(np.uint8_t[:, :, :] array, int true_height, int true_width, int block_size):
 
     result = np.zeros((true_height, true_width,4), dtype=np.uint8)
@@ -11,19 +13,17 @@ def fast_detemplatize(np.uint8_t[:, :, :] array, int true_height, int true_width
     
     cdef np.uint8_t red, green, blue, alpha
     
-    cdef Py_ssize_t x, y, b, b_y, b_x
+    cdef int x, y, b_y, b_x
 
-    for x in range(true_width):
-        for y in range(true_height):
-            for b in range(block_size*block_size):
-                b_x = b%block_size
-                b_y = b//block_size
-                alpha = array[y*block_size+b_y, x*block_size+b_x, 3]
-                if alpha!=0:
-                    result_view[y,x,0] = array[y*block_size+b_y, x*block_size+b_x, 0]
-                    result_view[y,x,1] = array[y*block_size+b_y, x*block_size+b_x, 1]
-                    result_view[y,x,2] = array[y*block_size+b_y, x*block_size+b_x, 2]
-                    result_view[y,x,3] = alpha
-                    break
+    for y in prange(block_size*true_height, nogil=True):
+        for x in range(block_size*true_width):
+            alpha = array[y, x, 3]
+            if alpha!=0:
+                b_y = y//block_size
+                b_x = x//block_size
+                result_view[b_y,b_x,0] = array[y, x, 0]
+                result_view[b_y,b_x,1] = array[y, x, 1]
+                result_view[b_y,b_x,2] = array[y, x, 2]
+                result_view[b_y,b_x,3] = alpha
 
     return result
